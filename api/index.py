439daca519341668372fe07e4a6a6e7821e2d950
Iani Robota <iamarobota@gmail.com>
2025-03-23 11:19:01 +0100

    0th version for lp testing

--- api/index.py
+++ api/index.py
@@ -2,6 +2,7 @@ import time
 import datetime as dt
 import json as JSON
 import  logging, sys
+from collections import Counter
 
 from flask import Flask,request,render_template
 logging.basicConfig(stream=sys.stdout,
@@ -28,13 +29,9 @@ def button():
     btnindex=post.get('btnindex')
     name=post.get('name')
     logger.info("post event {} from button {}".format(event,name))
-    data={"time":dt.datetime.now().isoformat(), "event":event, "name":name,"btnindex":btnindex}
+    data={"receive_time":dt.datetime.now().isoformat(), "event":event, "name":name,"btnindex":str(btnindex)}
     app_command=JSON.dumps(data)
-    home()
-    
-@app.route('/about')
-def about():
-    return 'About<br>command proxy'
+    return home()
 
 @app.route('/longpoll')
 def poll():
@@ -49,5 +46,22 @@ def poll():
     apc_save=app_command
     app_command=""
     t1=dt.datetime.now().isoformat()
-    data={"start":t0,"finish":t1,"pollcnt":ctr,"command":apc_save}
+    if apc_save!="":
+        command=JSON.loads(apc_save)
+        logger.info(JSON.dumps(command))
+        data0={"start":t0, "finish":t1, "pollcnt":str(ctr), "commands":"True"}
+        logger.info(JSON.dumps(data0))
+        data= {i: command.get(i, "") + data0.get(i, "") 
+                for i in set(command).union(data0)}
+    else:
+        data ={"start":t0, "finish":t1, "pollcnt":str(ctr), "commands":"False"}
     return(JSON.dumps(data))
+
+def create_app_for_test(appcommand):
+    global app_command
+    app = Flask(__name__)
+    app.config.update(
+            TEMPLATES_AUTO_RELOAD=True
+        )
+    app_command=appcommand
+    return app
