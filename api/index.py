e787b965191c618a8858d53d4806972959e5e4a0
Iani Robota <iamarobota@gmail.com>
2025-03-23 09:28:48 +0100

    Fw

--- api/index.py
+++ api/index.py
@@ -1,20 +1,53 @@
 import time
 import datetime as dt
+import json as JSON
+import  logging, sys
 
-from flask import Flask
+from flask import Flask,request,render_template
+logging.basicConfig(stream=sys.stdout,
+level=logging.DEBUG,datefmt='%T %D',
+format='%(asctime)s %(levelname)s %(message)s')
+
+logger=logging.getLogger()
 
 app = Flask(__name__)
+app.config.update(
+        TEMPLATES_AUTO_RELOAD=True
+    )
+app_command=""
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('index.html')
 
+@app.route('/button',methods=['POST'])
+def button():
+    global app_command
+    post=request.json
+    event=post.get('event')
+    btnindex=post.get('btnindex')
+    name=post.get('name')
+    logger.info("post event {} from button {}".format(event,name))
+    data={"time":dt.datetime.now().isoformat(), "event":event, "name":name,"btnindex":btnindex}
+    app_command=JSON.dumps(data)
+    home()
+    
 @app.route('/about')
 def about():
-    return 'About'
+    return 'About<br>command proxy'
 
 @app.route('/longpoll')
 def poll():
-    t0=dt.datetime.now
-    time.sleep(30)
-    return(f"start:{t0}<br>I am polly now:{dt.datetime.now}")
+    global app_command
+    t0=dt.datetime.now().isoformat()
+    ctr=0
+    while app_command=="":
+        time.sleep(0.2)
+        ctr+=1
+        if ctr>150:
+            break
+    apc_save=app_command
+    app_command=""
+    t1=dt.datetime.now().isoformat()
+    data={"start":t0,"finish":t1,"pollcnt":ctr,"command":apc_save}
+    return(JSON.dumps(data))
